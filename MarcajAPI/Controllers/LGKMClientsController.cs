5ab8aff7d7db157edceac8b52c8637cf1d7142f9
Andrei Puscasu <puscasu.and@gmail.com>
2022-11-02 12:52:06 +0200

    api is going through changes

---
+++ MarcajAPI/Controllers/LGKMClientsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+using LGKDataAccess;
+
+namespace MarcajAPI.Controllers
+{
+    public class LGKMClientsController : ApiController
+    {
+        public List<LGKMarcajClient> Get()
+        {
+            using(lgkdatabaseEntities en = new lgkdatabaseEntities())
+            {
+                en.Configuration.ProxyCreationEnabled = false;
+                var a = en.LGKMarcajClients.ToList();
+                return a;
+            }
+        }
+    }
+}
